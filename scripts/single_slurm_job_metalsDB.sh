c43624bb023da3c7a373c08a4ba0329bd9a80635
davidecrs <davidecrs92@gmail.com>
2023-11-07 11:37:23 +0100

    Added slurm script

---
+++ scripts/single_slurm_job_metalsDB.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#SBATCH --job-name="metalsDB"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=davide.corso@unina.it
+#SBATCH --time=96:00:00
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=15G
+#SBATCH --output=/ibiscostorage/dcorso/flavia_migliaccio/metal_transporters/slurm_logs/single_job/slurm-%A.out
+#SBATCH --partition=parallel
+
+#
+# Copyright 2023 Davide Corso
+#
+
+
+
+r1=$1
+r2=$2 
+sample_name=$3
+exp=$4
+
+
+mifaser_database="/ibiscostorage/dcorso/flavia_migliaccio/metal_transporters/data_for_mifaser_db/metals_as_groups"
+
+echo "$exp  ----  $sample_name  ----  MetalsDB"
+
+folder_output="/ibiscostorage/dcorso/flavia_migliaccio/metal_transporters/results_metalsDB/${exp}/${sample_name}/"
+folder_reads="/ibiscostorage/dcorso/flavia_migliaccio/metal_transporters/reads_after_fastp/${exp}"
+
+read_r1="${folder_reads}/${r1}"
+read_r2="${folder_reads}/${r2}"
+
+mkdir -p "${folder_output}"
+
+singularity exec --bind ${mifaser_database}:${mifaser_database} mifaser_custom.simg mifaser \
+  -d $mifaser_database \
+  -l ${read_r1} ${read_r2} \
+  -i /repos/mifaser/mifaser/diamond/linux \
+  -t $SLURM_CPUS_PER_TASK \
+  -o $folder_output
+
+echo "$?"
+echo "$exp  ----  $sample_name  ----  MetalsDB"
+
---
+++ scripts/single_slurm_job_proteinFamDB.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --job-name="protFamDB"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=davide.corso@unina.it
+#SBATCH --time=96:00:00
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=15G
+#SBATCH --output=/ibiscostorage/dcorso/flavia_migliaccio/metal_transporters/slurm_logs/single_job/slurm-%A.out
+#SBATCH --partition=parallel
+
+#
+# Copyright 2023 Davide Corso
+#
+
+r1=$1
+r2=$2 
+sample_name=$3
+exp=$4
+
+
+mifaser_database="/ibiscostorage/dcorso/flavia_migliaccio/metal_transporters/data_for_mifaser_db/proteinFam_as_groups"
+
+
+echo "$exp  ----  $sample_name  ----  protFamDB"
+
+folder_output="/ibiscostorage/dcorso/flavia_migliaccio/metal_transporters/results_proteinFamDB/${exp}/${sample_name}/"
+folder_reads="/ibiscostorage/dcorso/flavia_migliaccio/metal_transporters/reads_after_fastp/${exp}"
+
+read_r1="${folder_reads}/${r1}"
+read_r2="${folder_reads}/${r2}"
+
+mkdir -p "${folder_output}"
+
+singularity exec --bind ${mifaser_database}:${mifaser_database} mifaser_custom.simg mifaser \
+  -d $mifaser_database \
+  -l ${read_r1} ${read_r2} \
+  -i /repos/mifaser/mifaser/diamond/linux \
+  -t $SLURM_CPUS_PER_TASK \
+  -o $folder_output
+
+echo "$?"
+echo "$exp  ----  $sample_name  ----  protFamDB"
+
